244db11cd71b35168645864432eb7854760652f1
Heriberto Sosa <sosaheriberto2001@gmail.com>
2019-07-19 16:52:39 -0400

    adding dist

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 2000 } );
+    
+    $("#carouselButton").click(function(){
+    if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+        $("#mycarousel").carousel('pause');
+        $("#carouselButton").children("span").removeClass('fa-pause');
+        $("#carouselButton").children("span").addClass('fa-play');
+    }
+    else if ($("#carouselButton").children("span").hasClass('fa-play')){
+        $("#mycarousel").carousel('cycle');
+        $("#carouselButton").children("span").removeClass('fa-play');
+        $("#carouselButton").children("span").addClass('fa-pause');                    
+    }
+    });
+
+    $("#buttonModalLogin").click(function(){
+
+        $('#loginModal').modal('show');
+
+     });
+
+     $("#buttonModalReserve").click(function(){
+
+    $('#ReserveModal').modal('show');
+
+    });
+});
